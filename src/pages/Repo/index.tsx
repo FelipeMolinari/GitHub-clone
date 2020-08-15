21ea0910b99459e6aa22ed59ad42a21c735e8e73
Felipe Molinari <felipemolinari874@gmail.com>
2020-08-15 11:04:22 -0300

    :sparkles: Project files structured

---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Repo: React.FC = () => {
+  return <View />;
+}
+
+export default Repo;
