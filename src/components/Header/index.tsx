6535a8885f67835e4179583b115eacf6a090905e
Felipe Molinari <felipemolinari874@gmail.com>
2020-08-16 22:03:53 -0300

    Form navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { Container, SearchForm, GitHubLogo } from './styles';
-
+import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const Header: React.FC = () => {
+	const [ search, setSearch ] = useState('');
+	const navigate = useNavigate();
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		navigate('/' + search.toLowerCase().trim());
+	};
 	return (
 		<Container>
 			<GitHubLogo />
-			<SearchForm>
-				<input placeholder="Enter a username or Repo" />
+			<SearchForm onSubmit={handleSubmit}>
+				<input
+					placeholder="Enter a username or Repo"
+					value={search}
+					onChange={(e) => setSearch(e.currentTarget.value)}
+				/>
 			</SearchForm>
 		</Container>
 	);
